fb7be35a0d97bda15c12b9dcb1fafa21b999e4d1
jenniferbogado1 <127226927+jenniferbogado1@users.noreply.github.com>
2025-03-07 23:53:33 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -6,22 +6,22 @@ document.addEventListener("DOMContentLoaded", () => {
             image: 'https://i.imgur.com/scsbxf4.jpeg'
         },
         {
-            name: '2 much - Jb',
+            name: '2 much - Justin Bieber',
             audio: 'https://www.dropbox.com/scl/fi/05hwg1g8sq818wep9fr4m/Justin-Bieber-2-Much-Visualizer-JustinBieberVEVO.mp3?rlkey=3jeg7djqg1ohzw4nk7cj2d1x9&st=6wjes4zl&dl=1',
             image: 'https://i.imgur.com/olia8iv.gif'
         },
         {
-            name: 'Canción 3',
+            name: 'Apocalypse - Cigarettes After Sex',
             audio: 'https://www.dropbox.com/scl/fi/gpu1mkzgz0dig8zhu3adu/Apocalypse-Cigarettes-After-Sex-Cigarettes-After-Sex-1.mp3?rlkey=s08zx8qwizd3m9g2t905a1urs&st=xnq213d8&dl=1',
             image: 'https://i.imgur.com/PrxIDOx.gif'
         },
         {
-            name: 'Canción 4',
+            name: 'We fell in love in October - girl in red',
             audio: 'https://www.dropbox.com/scl/fi/b58lh6rqq5ayp5felczyu/girl-in-red-we-fell-in-love-in-october-girl-in-red.mp3?rlkey=vqjnw4clph3yle7nhi7yfgfh2&st=x7696qkb&dl=1',
             image: 'https://i.imgur.com/rMM2IFB.gif'
         },
         {
-            name: 'Canción 5',
+            name: 'I wanna be yours - Arctic Monkeys',
             audio: 'https://www.dropbox.com/scl/fi/o4v89iucfe2hzpzlgcp4o/I-Wanna-Be-Yours-Arctic-Monkeys.mp3?rlkey=ngfkucd1gwxps5zfr5ys5oc6e&st=vo23t03f&dl=1',
             image: 'https://imgur.com/9Q53WwT.gif'
         }
